5434ccad7ce0cf1744c5a26f4c407c119ae313ef
Josefina Valencia <j.valencia04@ufromail.cl>
2026-09-09 23:42:20 -0300

    interfaz de login

--- src/main/java/org/example/VentanaLogin.java
+++ src/main/java/org/example/VentanaLogin.java
@@ -14,11 +14,32 @@ public class VentanaLogin {
     private final JButton btnIngresar = new JButton("Ingresar");
 
     public VentanaLogin() {
+
         USUARIOS.add(new Usuario("admin", "1234", "Administrador"));
         USUARIOS.add(new Usuario("jugador", "1234", "Jugador"));
+
+        frame.setSize(350, 250);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        lblUsuario.setBounds(30, 30, 80, 25);
+        txtUsuario.setBounds(120, 30, 180, 25);
+
+        lblClave.setBounds(30, 70, 80, 25);
+        txtClave.setBounds(120, 70, 180, 25);
+
+        btnIngresar.setBounds(120, 120, 180, 30);
+
+        frame.add(lblUsuario);
+        frame.add(txtUsuario);
+        frame.add(lblClave);
+        frame.add(txtClave);
+        frame.add(btnIngresar);
     }
 
     public void mostrarVentana() {
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     private void login() {
@@ -29,5 +50,10 @@ public class VentanaLogin {
     }
 
     private void abrirRegistro() {
+    }
+
+    public static void main(String[] args) {
+        VentanaLogin ventana = new VentanaLogin();
+        ventana.mostrarVentana();
     }
 }
